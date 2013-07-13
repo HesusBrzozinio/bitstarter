0fb8241f95d49148b7478900557fe2129039318c
HesusBrzozinio <kamilbrzozowski@gmail.com>
2013-07-13 06:31:36 +0000

    read file

--- web.js
+++ web.js
@@ -3,20 +3,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffLength = 64*1024;
-  var buff = new Buffer(buffLength);
-  var fdr = fs.openSync("index.html", "r");
-  var bytesRead = 1;
-  var pos = 0;
-
-  while(bytesRead > 0){
-   bytesRead = fs.readSync(fdr, buff, 0, buffLength, pos);
-   pos += bytesRead;
-  }
-
-  fs.closeSync(fdr);
-  
-  response.send(buff.toString('utf8', 0, buff.length));
+  var content = fs.ReadFileSync('index.html').toString();  
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
