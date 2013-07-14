55d5423e8ba5ca9f1599854f21e63face779a15e
HesusBrzozinio <kamilbrzozowski@gmail.com>
2013-07-14 17:46:48 +0000

    parsing url

--- grader.js
+++ grader.js
@@ -22,10 +22,13 @@ References:
 */
 
 var fs = require('fs');
+var sys = require('util');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://mysterious-fortress-7853.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,8 +68,28 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_address>', 'URL address', URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+
+    var checkJson;
+    if(program.url != null){
+       //console.log("downloading file from url");
+       rest.get(program.url).on('complete', function(result) {
+           if (result instanceof Error) {
+              sys.puts('Error: ' + result.message);
+              this.retry(5000); // try again after 5 sec
+           } else {
+              //sys.puts(result);
+              fs.writeFileSync('page.html', result);
+           }
+       });
+       //console.log("parsing file from url");
+       checkJson =  checkHtmlFile('page.html', program.checks);
+    }else{
+       //console.log("parsing local file");
+       checkJson = checkHtmlFile(program.file, program.checks);
+    }    
+
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
